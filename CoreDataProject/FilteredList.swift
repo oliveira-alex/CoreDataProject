b80ef7c856be33e7ce0e218ed219d1b8af5262f4
ALeX <alexdeoliveira.poli@gmail.com>
2021-10-04 19:39:14 -0300

    Dynamically filtering @FetchRequest with SwiftUI – Core Data SwiftUI Tutorial 6/7 part1

---
+++ CoreDataProject/FilteredList.swift
@@ -0,0 +1,23 @@
+//
+//  FilteredList.swift
+//  CoreDataProject
+//
+//  Created by Alex Oliveira on 04/10/21.
+//
+
+import SwiftUI
+
+struct FilteredList: View {
+    var fetchRequest: FetchRequest<Singer>
+    var singers: FetchedResults<Singer> { fetchRequest.wrappedValue }
+    
+    var body: some View {
+        List(singers, id: \.self) { singer in
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        }
+    }
+    
+    init(filter: String) {
+        fetchRequest = FetchRequest<Singer>(entity: Singer.entity(), sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    }
+}
---
+++ CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Alex Oliveira on 04/10/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
